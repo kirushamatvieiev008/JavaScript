5a30b9791a2a0c809c00883266827495a9c0244a
Кірюша <mkiril087@gmail.com>
2024-08-06 21:44:52 +0300

    gh

---
+++ index.js
@@ -0,0 +1,28 @@
+// let y = 5;
+// let x = 21;
+
+// let p = x + y;
+
+// console.log(p);
+
+
+let x = "Українізація москалів";
+let y = 7;
+let p = true;
+let q = false;
+let t = null;
+let i;
+// console.log(i);
+// typeof використовується для отримання типу змінної
+console.log(typeof i);
+// alert можна виводити якийсь текст у маленьке вiконце з однією кнопкою ок
+// alert(x)
+
+// const username = "Kiril";
+// console.log("username is", username)
+
+// отримання данних
+// const hotel = confirm("чи справдi ви хочете забронювати номер?")
+
+const hotelName = prompt('напишіть будьласка назву готелю')
+console.log(hotelName)
