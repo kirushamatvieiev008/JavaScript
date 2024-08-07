5a761be96228d24779ad2d21cc288250343041db
Кірюша <mkiril087@gmail.com>
2024-08-07 22:04:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,15 @@
 // console.log(p);
 
 
-let x = "Українізація москалів";
-let y = 7;
-let p = true;
-let q = false;
-let t = null;
-let i;
+// let x = "Українізація москалів";
+// let y = 7;
+// let p = true;
+// let q = false;
+// let t = null;
+// let i;
 // console.log(i);
 // typeof використовується для отримання типу змінної
-console.log(typeof i);
+// console.log(typeof i);
 // alert можна виводити якийсь текст у маленьке вiконце з однією кнопкою ок
 // alert(x)
 
@@ -24,5 +24,26 @@ console.log(typeof i);
 // отримання данних
 // const hotel = confirm("чи справдi ви хочете забронювати номер?")
 
-const hotelName = prompt('напишіть будьласка назву готелю')
-console.log(hotelName)
+// const hotelName = prompt('напишіть будьласка назву готелю')
+// console.log(hotelName)
+
+
+// Реєстрація користувача
+
+let x = "ми склали про вас таку думку: ви людина";
+let y = "якщо ні то нам всеодно і нікуди ви не напишете на цьому сайті :)"
+let danni = "ваші данні:";
+const userName = prompt('як вас звати?');
+
+console.log(userName);
+// const realName = confirm(`чи справді вас звати ${userName}?`)
+const userAge = prompt('скільки вам років?☠️');
+const mail = prompt(`напишіть вашу пошту`);
+const tel = prompt('напишіть ваш номер телефону');
+const city = prompt('який ваш населений пункт?');
+const login = prompt('придумайте ваш логін');
+const password = prompt('придумайте ваш пароль');
+let messageFirst = `${danni}, ${userName}, ${userAge}, ${mail}, ${tel}, ${city}, ${login}, ${password}`;
+// let message = `ваші данні: ${userNamelName}, ${userAge}, ${mail}, ${tel}, ${city}, ${login}, ${password}.`;
+alert(`${messageFirst}`);
+// alert(`${x}, ${y}`);
